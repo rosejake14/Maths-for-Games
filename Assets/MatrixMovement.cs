8ed5de655f7972a8530fe81fb5f70c6cc13248ea
rosejake14 <62072145+rosejake14@users.noreply.github.com>
2024-02-20 12:06:12 +0000

    Rotations + Editor Stuff!

--- Assets/MatrixMovement.cs
+++ Assets/MatrixMovement.cs
@@ -14,9 +14,21 @@ public class MatrixMovement : MonoBehaviour
 
     [SerializeField]
     float yawAngle;
-
+    [SerializeField]
+    float rollAngle;
+    [SerializeField]
+    float pitchAngle;
     [SerializeField]
     float scale = 2.0f;
+    [SerializeField]
+    float rotationSpeed = 1.0f;
+
+    [SerializeField]
+    bool LinearInterpolate = false;
+    [SerializeField]
+    float LinearInterpolateSpeed = 1.0f;
+
+    
     void Start()
     {
         MeshFilter meshFilter = GetComponent<MeshFilter>();
@@ -26,44 +38,94 @@ public class MatrixMovement : MonoBehaviour
 
     void Update()
     {
-        yawAngle += Time.deltaTime * 1;
-        //transform.position = MathLib.LinInterpolate(transform.position, TargetLocation, Time.deltaTime * 1);
-
-        Vector3[] TransformedVertices = new Vector3[ModelSpaceVertices.Length];
-
-        //ROTATION
-        Matrix4by4 rotMatrix = new Matrix4by4(
-            new Vector3(Mathf.Cos(yawAngle), 0, -Mathf.Sin(yawAngle)),
-            new Vector3(0, 1, 0),
-            new Vector3(Mathf.Sin(yawAngle), 0, Mathf.Cos(yawAngle)),
-            Vector3.zero
-            );
-
-        //SCALE
-        Matrix4by4 scaleMatrix = new Matrix4by4(new Vector3(1, 0, 0) * scale, new Vector3(0, 1, 0) * scale, new Vector3(0, 0, 1) * scale, Vector3.zero);
-
-        //TRANSLATION
-        Matrix4by4 transMatrix = new Matrix4by4(
-            new Vector3(1, 0, 0),
-            new Vector3(0, 1, 0),
-            new Vector3(0, 0, 1),
-            new Vector3(MatrixTargetLocation.x, MatrixTargetLocation.y, MatrixTargetLocation.z));
-
-
-        for (int i = 0; i < TransformedVertices.Length; i++)
+        if (!LinearInterpolate)
         {
-            Vector4 TranslationModelSpaceV = new Vector4(ModelSpaceVertices[i].x, ModelSpaceVertices[i].y, ModelSpaceVertices[i].z, 1);
-            TransformedVertices[i] = transMatrix * TranslationModelSpaceV;
-            //TransformedVertices[i] = scaleMatrix * TransformedVertices[i];
-            //TransformedVertices[i] = rotMatrix * TransformedVertices[i];
+            yawAngle += Time.deltaTime * rotationSpeed;
+            pitchAngle += Time.deltaTime * rotationSpeed;
+            rollAngle += Time.deltaTime * rotationSpeed;
+
+            Vector3[] TransformedVertices = new Vector3[ModelSpaceVertices.Length];
+
+            //ROTATION
+            //ROLL
+            Matrix4by4 rollMatrix = new Matrix4by4(
+                new Vector3(Mathf.Cos(rollAngle), Mathf.Sin(rollAngle), 0),
+                new Vector3(-Mathf.Sin(rollAngle), Mathf.Cos(rollAngle), 0),
+                new Vector3(0, 0, 1),
+                Vector3.zero
+                );
+
+            //PITCH
+            Matrix4by4 pitchMatrix = new Matrix4by4(
+                new Vector3(1, 0, 0),
+                new Vector3(0, Mathf.Cos(pitchAngle), Mathf.Sin(pitchAngle)),
+                new Vector3(0, -Mathf.Sin(pitchAngle), Mathf.Cos(pitchAngle)),
+                Vector3.zero
+                );
+
+            //YAW
+            Matrix4by4 yawMatrix = new Matrix4by4(
+                new Vector3(Mathf.Cos(yawAngle), 0, -Mathf.Sin(yawAngle)),
+                new Vector3(0, 1, 0),
+                new Vector3(Mathf.Sin(yawAngle), 0, Mathf.Cos(yawAngle)),
+                Vector3.zero
+                );
+
+
+            //SCALE
+            Matrix4by4 scaleMatrix = new Matrix4by4(new Vector3(1, 0, 0) * scale, new Vector3(0, 1, 0) * scale, new Vector3(0, 0, 1) * scale, Vector3.zero);
+
+            //TRANSLATION
+            Matrix4by4 transMatrix = new Matrix4by4(
+                new Vector3(1, 0, 0),
+                new Vector3(0, 1, 0),
+                new Vector3(0, 0, 1),
+                new Vector3(MatrixTargetLocation.x, MatrixTargetLocation.y, MatrixTargetLocation.z));
+
+
+            for (int i = 0; i < TransformedVertices.Length; i++)
+            {
+                //TRANSLATE ROTATE SCALE
+                //TRANSLATE
+                Vector4 TranslationModelSpaceV = new Vector4(ModelSpaceVertices[i].x, ModelSpaceVertices[i].y, ModelSpaceVertices[i].z, 1);
+                TransformedVertices[i] = transMatrix * TranslationModelSpaceV;
+
+                //ROTATE
+                //TransformedVertices[i] = rotMatrix * TransformedVertices[i];
+                Vector3 RolledV = rollMatrix * TransformedVertices[i];
+                Vector3 PitchedV = pitchMatrix * RolledV;
+                Vector3 YawedV = yawMatrix * PitchedV;
+                TransformedVertices[i] = YawedV;
+
+                //SCALE
+                TransformedVertices[i] = scaleMatrix * TransformedVertices[i];
+            }
+
+            MeshFilter meshFilter = GetComponent<MeshFilter>();
+
+            meshFilter.mesh.vertices = TransformedVertices;
+
+            meshFilter.mesh.RecalculateNormals();
+            meshFilter.mesh.RecalculateBounds();
         }
+        else
+        {
+            if(transform.position != TargetLocation)
+            {
+                transform.position = MathLib.LinInterpolate(transform.position, TargetLocation, Time.deltaTime * LinearInterpolateSpeed);
+            }
+            else
+            {
+                TargetLocation = newLinLocation();
+            }
+        }
+    }
 
-        MeshFilter meshFilter = GetComponent<MeshFilter>();
 
-        meshFilter.mesh.vertices = TransformedVertices;
+    Vector3 newLinLocation()
+    {
+        Vector3 rv = new Vector3(Random.Range(-10,10), Random.Range(0, 10), Random.Range(-10, 10));
 
-        meshFilter.mesh.RecalculateNormals();
-        meshFilter.mesh.RecalculateBounds();
-
+        return rv;
     }
 }
